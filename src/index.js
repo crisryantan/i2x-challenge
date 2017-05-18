93d27d90325766c2aaa3f9549792905172dee29e
Cris Ryan Tan <crisryantan@gmail.com>
2017-05-18 18:18:35 +0800

    initial commit

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './containers/App';
+import './global.css';
+
+ReactDOM.render(
+  <App />,
+  document.querySelector( '#root' )
+);
